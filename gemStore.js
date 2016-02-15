8181d7b588000f1fedd294c0fc037d94ff8646ee
Lucas Nesci <nescilucas@gmail.com>
2016-02-15 15:36:46 -0300

    Clase 1 - Ejericio 3

--- gemStore.js
+++ gemStore.js
@@ -1,9 +1,14 @@
 (function() {
-  var gem = { name: 'Azurite', price: 2.95 };
+  var gem = { name: 'Azurite', price: 2.95, canPurchase: true, soldOut: false };
 
   angular
     .module('gemStore', [])
-    .controller('StoreController', function() {
+    .controller('StoreController', function($log) {
       this.product = gem;
+      this.purchase = purchase;
+
+      function purchase() {
+        $log.warn('StoreController.purchase NOT_IMPLEMENTED');
+      }
     });
 })()
