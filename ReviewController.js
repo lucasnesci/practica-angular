b804920d5d110d453904f54a8f09b54deea473f3
Lucas Nesci <nescilucas@gmail.com>
2016-02-22 16:10:50 -0300

    Clase 3 - Ejericio 4 y 5

---
+++ ReviewController.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('gemStore')
+    .controller('ReviewController', ReviewController);
+
+    function ReviewController() {
+      var vm = this;
+      vm.addReview = addReview;
+      reset();
+
+      function reset() {
+        vm.review = {};
+      }
+
+      function addReview(product) {
+        product.reviews.push(vm.review);
+        reset();
+      }
+    };
+})();
