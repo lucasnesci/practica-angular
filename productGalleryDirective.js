5bb3ee0aedef107de3d91859a9afa076a4f9e720
Lucas Nesci <nescilucas@gmail.com>
2016-02-29 16:12:39 -0300

    Clase 4 - Ejericio 5

---
+++ productGalleryDirective.js
@@ -0,0 +1,28 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('gemStore')
+    .directive('productGallery', productGallery);
+
+  function productGallery() {
+    var directive = {
+      restrict: 'E',
+      templateUrl: 'product-gallery.html',
+      controller: GalleryController,
+      controllerAs: 'gallery'
+    }
+
+    return directive;
+  }
+
+  function GalleryController() {
+    var vm = this;
+    vm.current = 0;
+    vm.setCurrent = setCurrent;
+
+    function setCurrent(current) {
+        vm.current = current || 0;
+    }
+  }
+})();
